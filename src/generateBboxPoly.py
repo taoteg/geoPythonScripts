da55291462dc032ab8a479f66e5ceaa6dc1c9e89
taoteg <taoteg@gmail.com>
2017-11-22 12:31:13 -0600

    Cleaning up test script. Working on clean base script for final version.

--- src/generateBboxPoly.py
+++ src/generateBboxPoly.py
@@ -4,47 +4,47 @@ Uses bounding box SW & NE lat long decimal degrees coordinates as inputs.
 """
 
 # Module imports.
+import os
 import shapefile as shp
+from pathlib import Path
 
 # Module Vars
-# TEST COORDS: -97.546649, 32.550058 & -97.034774, 32.987978
 SW_LAT = 32.550058
 SW_LONG = -97.546649
 NE_LAT = 32.987978
 NE_LONG = -97.034774
 # cellWidth = 10
 # cellHeight = 10
+
 fileBaseName = 'bbox_polygon'
 
-# Example.
-# """
+current_dir = os.getcwd()
+print('current_dir :: ', current_dir)
+absolute_path = os.path.abspath('.')
+print('absolute_path :: ', absolute_path)
+base_path = Path(".").resolve()
+print('base_path :: ', base_path)
+subdir_path = 'shapefiles/generateBBoxPoly/'
+print('subdir_path :: ', subdir_path)
+output_path = os.path.join(str(base_path), str(subdir_path))
+print('output_path :: ', output_path)
+
+# Generate Shapefile.
+output_shapefile = os.path.join(output_path, fileBaseName)
+print('output_shapefile :: ', output_shapefile)
 w = shp.Writer(shp.POLYGON)
 w.autoBalance = 1
-# w.poly(parts=[[[1,5],[5,5],[5,1],[3,3],[1,1]]])
-# [[SW_LAT, SW_LONG], [NE_LAT, SW_LONG], [NE_LAT, NE_LONG], [SW_LAT, NE_LONG], [SW_LAT, SW_LONG]]
 w.poly(parts=[[[SW_LAT, SW_LONG], [NE_LAT, SW_LONG], [NE_LAT, NE_LONG], [SW_LAT, NE_LONG], [SW_LAT, SW_LONG]]])
 w.field('FIRST_FLD','C','100')
 w.field('SECOND_FLD','C','100')
 w.record('First','Polygon')
-# w.save('shapefiles/test/polygon')
-w.save(fileBaseName)
+w.save(output_shapefile)
 
-# Projection file.
-prj = open("%s.prj" % fileBaseName, "w")
+# Generate Projection file.
+output_projection = os.path.join(output_path, fileBaseName)
+print('output_projection :: ', output_projection)
+# prj = open("%s.prj" % fileBaseName, "w")
+prj = open("%s.prj" % output_projection, "w")
 epsg = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]'
 prj.write(epsg)
 prj.close()
-# """
-
-# Generate grid file.
-"""
-w = shp.Writer(shp.POLYGON)
-w.autoBalance = 1
-w.field('ID')
-w.field('NAME')
-# id = 0
-# feature_name = ""
-# w.line(parts=[[[1,5],[5,5],[5,1],[3,3],[1,1]]])
-w.poly(shapeType=3, parts=[[[122,37,4,9], [117,36,3,4]], [[115,32,8,8], [118,20,6,4], [113,24]]])
-w.save(fileBaseName)
-"""
